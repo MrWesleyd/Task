0abef64fea079640547f3ab91cb6e30a48d08609
MrWesleyd <107256646+MrWesleyd@users.noreply.github.com>
2022-06-16 17:49:06 +0200

    Add files via upload

---
+++ Задание вступ/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0}", Chocolate(15));
+            Console.ReadKey();
+        }
+        static int Chocolate(int x)
+        {
+            int konec = 0, obertka = 0;
+
+            while (x > 0)
+            {
+                x--;
+                obertka++;
+                if (obertka == 3)
+                {
+                    obertka = 0;
+                    x++;
+                }
+                konec++;
+            }
+            return konec;
+        }
+    }
+}
